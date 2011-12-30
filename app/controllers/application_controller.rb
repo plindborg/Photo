febe9227aa39f48fd3f2f2e034a6cc130d5f1efe
Patrick Lindborg <lindborg@gmail.com>
2011-12-30 22:48:09 +0100

    Did some work in images#show, and some layout work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,23 @@
 class ApplicationController < ActionController::Base
 	helper_method :getCurrentUser?
+	helper_method :isLoggedIn?
   protect_from_forgery
 	before_filter :authorize
 
 	def getCurrentUser?
 		user = User.find_by_id(session[:user_id])
-		if user == nil
-			"guest"
+		if user.nil?
+			"Guest"
 		else
 			 user.name
 		end
+	end
+
+	def isLoggedIn?
+		if session[:user_id].nil?
+			return true
+		end
+		return false
 	end
 
 	protected
